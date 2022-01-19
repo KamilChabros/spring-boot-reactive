d4ec51426fb3402c393ba7909a71bf17d9dada00
KamilChabros <kamilchabros@wp.pl>
2022-01-19 22:12:39 +0100

    Stock client module added

---
+++ stockclient/src/test/java/xyz/reactivespringboot/demo/stockclient/StockclientApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.reactivespringboot.demo.stockclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
